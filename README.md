d26ba7796b8e03bf0653a4bc8b91e143c52d9b22
TranPhucNguyen-UMT <nhoxnguyenpk@gmail.com>
2026-03-17 13:38:23 +0700

    clear all files

--- README.md
+++
@@ -1,1 +0,0 @@
-# PC_store-mobile-web-soft
--- services/payment-s.js
+++
@@ -1,8 +0,0 @@
-import Payment from '../models/payment.js';
-import AppError from '../utils/appError.js';
-
-export const processPaymentWebhook = async (paymentId, status, transactionCode) => {
-  const updated = await Payment.updateStatus(paymentId, status, transactionCode);
-  if (!updated) throw new AppError('Không tìm thấy thông tin thanh toán', 404);
-  return { paymentId, status };
-};
