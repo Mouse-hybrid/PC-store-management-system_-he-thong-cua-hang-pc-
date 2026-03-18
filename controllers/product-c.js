4a3f130c6ef1239c2d5dbf06cc88fd157a375d9a
Mouse-hybrid <nguyenhadangkhoa232@gmail.com>
2026-03-18 19:21:03 +0700

    Đã sửa xung đột và giữ nguyên code chuẩn từ máy

--- controllers/product-c.js
+++ controllers/product-c.js
@@ -11,14 +11,9 @@ export const getAllProducts = async (req, res, next) => {
 
 export const searchProducts = async (req, res, next) => {
   try {
-<<<<<<< HEAD
     const { q } = req.query; 
     // SỬA: Gọi getFilteredProducts và truyền object chứa q
     const results = await productService.getFilteredProducts({ q }); 
-=======
-    const { q } = req.query; // q là keyword từ khách hàng
-    const results = await productService.searchProducts(q);
->>>>>>> f42558b2c199dd3e958fcd5af79d3c8e84e58a21
     res.list(results, { keyword: q });
   } catch (err) {
     next(err);
